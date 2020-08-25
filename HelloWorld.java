4fb8e6d13b781d4c718a82e1539260d59955268f
Initzzh <70217770+Initzzh@users.noreply.github.com>
2020-08-26 00:47:18 +0800

    优化代码
    一点点修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ package java1.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("你好，世界；HelloWorld");
+		System.out.println("你好，GitHub；HelloWorld");
 	}
 
 }
